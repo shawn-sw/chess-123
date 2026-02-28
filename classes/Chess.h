de059d648f1ef962c00f9d9f60e07004264c4761
shawn-sw <146413305+shawn-sw@users.noreply.github.com>
2026-02-28 04:01:56 -0800

    Update Chess.h

--- classes/Chess.h
+++ classes/Chess.h
@@ -27,6 +27,8 @@ public:
     bool canBitMoveFrom(Bit &bit, BitHolder &src) override;
     bool canBitMoveFromTo(Bit &bit, BitHolder &src, BitHolder &dst) override;
     bool actionForEmptyHolder(BitHolder &holder) override;
+    void bitMovedFromTo(Bit &bit, BitHolder &src, BitHolder &dst) override;
+    void pieceTaken(Bit *bit) override;
 
     void stopGame() override;
 
@@ -44,6 +46,18 @@ private:
     Player* ownerAt(int x, int y) const;
     void FENtoBoard(const std::string& fen);
     char pieceNotation(int x, int y) const;
+    ChessPiece pieceTypeForBit(const Bit& bit) const;
+    bool isPathClear(int srcX, int srcY, int dstX, int dstY) const;
+    bool canPawnMove(const Bit& bit, const ChessSquare& src, const ChessSquare& dst) const;
+    bool canCastle(const Bit& bit, const ChessSquare& src, const ChessSquare& dst) const;
+    bool isSquareUnderAttack(int x, int y, int attackingPlayer) const;
+    bool pieceAttacksSquare(const Bit& bit, const ChessSquare& src, int targetX, int targetY) const;
+    void markCastlingStateForMove(const Bit& bit, const ChessSquare& src);
+    void markCastlingStateForCapturedPiece(const Bit& bit, const ChessSquare& square);
 
     Grid* _grid;
+    bool _kingMoved[2];
+    bool _rookMoved[2][2];
+    int _enPassantTargetX;
+    int _enPassantTargetY;
 };
